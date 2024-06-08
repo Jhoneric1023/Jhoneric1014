be7386dcb41b36b07267e1a7d362638c8c926aa1
Jhoneric1023 <jero79721@gmail.com>
2024-06-08 15:14:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const btnLike1 = document.getElementById("btnLike1")
 const countLikes1 = document.getElementById("countLikes1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
 function clickLike1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
